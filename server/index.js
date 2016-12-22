f0ea747a34eae2de7f5a6ea9f984ae516df3ba5c
Jim Cummins <jimthedev@gmail.com>
2016-12-22 12:49:06 -0600

    remove dotenv in production but for real this time

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config(); // environment variables, used for hiding secrets
 }
 var express = require('express'); // making apis, a webserver
